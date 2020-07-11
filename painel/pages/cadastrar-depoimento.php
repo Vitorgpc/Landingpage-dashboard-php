aaee601727320af75160c62af089cca8d7aa14c0
Vitorgpc <vitorcolazelli@gmail.com>
2020-07-10 22:36:09 -0300

    Inserção de dados dinamica

---
+++ painel/pages/cadastrar-depoimento.php
@@ -0,0 +1,31 @@
+<?php 
+    verificaPermissaoPagina(2);
+?>
+
+<div class="box-content">
+    <h2><i class="fas fa-file-alt"></i> Cadastrar Depoimentos</h2>
+    <form method="post">
+        <?php
+            if(isset($_POST['acao'])){
+                if(Painel::insert($_POST)){
+                    Painel::alerta('sucesso', ' O cadastro foi realizado com sucesso');
+                } else{
+                    Painel::alerta('erro', ' Campos vazios nao sao permitidos');
+                }
+            }
+        ?>
+
+        <div class="form-group">
+            <label>Nome da pessoa: </label>
+            <input type="text" name="nome" />
+        </div>
+        <div class="form-group">
+            <label>Depoimento: </label>
+            <textarea name="depoimento"></textarea>
+        </div>
+        <div class="form-group">
+            <input type="hidden" name="nome_tabela" value="tb_site.depoimentos"/>
+            <input type="submit" name="acao" value="Cadastrar!" />
+        </div>
+    </form>
+</div>
