d4f348fa2e9cf3ff0bb46ac8da2be10a1cbc0470
Vitorgpc <vitorcolazelli@gmail.com>
2020-07-13 14:14:48 -0300

    Portal de noticias

---
+++ pages/noticias.php
@@ -0,0 +1,77 @@
+<section class="header-noticias">
+    <div class="center">
+        <h2><i class="far fa-newspaper"></i></h2>
+        <h2>Acompanhe as ultimas <b>noticias do portal</b></h2>
+    </div>
+</section>
+
+<section class="container-portal">
+    <div class="center">
+        <div class="sidebar">
+            <div class="box-content-sidebar">
+                <h3><i class="fa fa-search"></i> Realizar uma Busca:</h3>
+                <form action="">
+                    <input type="text" name="busca" placeholder="o que deseja procurar?" required/>
+                    <input type="submit" name="acao" value="Pesquisa!"/>
+                </form>
+            </div>
+            <div class="box-content-sidebar">
+                <h3><i class="fas fa-list-ul"></i> Selecione a Categoria:</h3>
+                <form action="">
+                    <select name="categoria">
+                        <option value="esportes">Esportes</option>
+                        <option value="esportes">Esportes</option>
+                    </select>
+                </form>
+            </div>
+            <div class="box-content-sidebar">
+                <h3><i class="fa fa-user"></i> Sobre o autor:</h3>
+                <div class="autor-box-portal">
+                    <div class="box-img-autor"></div>
+                    <div class="texto-autor-portal text-center">
+                        <h3>Guilherme C. Grillo</h3>
+                        <p>Nunc pretium, lectus sit amet imperdiet commodo, velit ex rutrum massa, quis mollis tellus nibh a tellus. Nullam euismod mi id mi consequat elementum. Nulla non fringilla nisl, nec scelerisque arcu. Donec aliquet sapien tempor erat rhoncus gravida. Morbi a magna eros. Sed sit amet ante semper, fringilla lacus vitae, bibendum urna. Pellentesque nec sagittis libero. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="conteudo-portal">
+            <div class="topo-conteudo-portal">
+                <!--<h2>Visualizando todos os Posts</h2>-->
+                <h2>Visualizando posts em <span>Esportes</span></h2>
+            </div>
+            <div class="box-single-conteudo">
+                <h2>19/09/2008 - Conheça os eleitos para ga...</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris felis enim, tristique in nisl et, eleifend tristique sem. Aliquam in tortor euismod, cursus justo id, consequat metus. Ut metus urna, finibus imperdiet sem eu, lacinia semper enim. Praesent dictum ipsum in orci vulputate, ut venenatis nunc fringilla.</p>
+                <a href="<?php echo INCLUDE_PATH; ?>esportes/nome-do-post">Leia Mais</a>
+            </div>
+            <div class="box-single-conteudo">
+                <h2>19/09/2008 - Conheça os eleitos para ga...</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris felis enim, tristique in nisl et, eleifend tristique sem. Aliquam in tortor euismod, cursus justo id, consequat metus. Ut metus urna, finibus imperdiet sem eu, lacinia semper enim. Praesent dictum ipsum in orci vulputate, ut venenatis nunc fringilla.</p>
+                <a href="<?php echo INCLUDE_PATH; ?>esportes/nome-do-post">Leia Mais</a>
+            </div>
+            <div class="box-single-conteudo">
+                <h2>19/09/2008 - Conheça os eleitos para ga...</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris felis enim, tristique in nisl et, eleifend tristique sem. Aliquam in tortor euismod, cursus justo id, consequat metus. Ut metus urna, finibus imperdiet sem eu, lacinia semper enim. Praesent dictum ipsum in orci vulputate, ut venenatis nunc fringilla.</p>
+                <a href="<?php echo INCLUDE_PATH; ?>esportes/nome-do-post">Leia Mais</a>
+            </div>
+            <div class="box-single-conteudo">
+                <h2>19/09/2008 - Conheça os eleitos para ga...</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris felis enim, tristique in nisl et, eleifend tristique sem. Aliquam in tortor euismod, cursus justo id, consequat metus. Ut metus urna, finibus imperdiet sem eu, lacinia semper enim. Praesent dictum ipsum in orci vulputate, ut venenatis nunc fringilla.</p>
+                <a href="<?php echo INCLUDE_PATH; ?>esportes/nome-do-post">Leia Mais</a>
+            </div>
+            <div class="box-single-conteudo">
+                <h2>19/09/2008 - Conheça os eleitos para ga...</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris felis enim, tristique in nisl et, eleifend tristique sem. Aliquam in tortor euismod, cursus justo id, consequat metus. Ut metus urna, finibus imperdiet sem eu, lacinia semper enim. Praesent dictum ipsum in orci vulputate, ut venenatis nunc fringilla.</p>
+                <a href="<?php echo INCLUDE_PATH; ?>esportes/nome-do-post">Leia Mais</a>
+            </div>
+            <div class="paginator">
+                <a class="active-page" href="">1</a>
+                <a href="">2</a>
+                <a href="">3</a>
+            </div>
+        </div>
+        <div class="clear"></div>
+    </div>
+</section>
