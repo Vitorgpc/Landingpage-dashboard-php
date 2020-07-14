35ed3b30544bfc2ce4d3f1d100df4a7e802fe22f
Vitorgpc <vitorcolazelli@gmail.com>
2020-07-14 12:36:16 -0300

    config de users online

--- classes/Site.php
+++ classes/Site.php
@@ -12,7 +12,13 @@
                     $sql = MySql::conectar()->prepare("UPDATE `tb_admin.online` SET ultima_acao = ? WHERE token = ?");
                     $sql->execute(array($horarioAtual, $token));
                 } else{
-                    $ip = $_SERVER['REMOTE_ADDR'];
+                    if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+                        $ip = $_SERVER['HTTP_CLIENT_IP'];
+                    } else if(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+                        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+                    } else{
+                        $ip = $_SERVER['REMOTE_ADDR'];
+                    }
                     $token = $_SESSION['online'];
                     $horarioAtual = date('Y-m-d H:i:s');
                     $sql = MySql::conectar()->prepare("INSERT INTO `tb_admin.online` VALUES (null,?,?,?)");
@@ -20,7 +26,13 @@
                 }
             } else{
                 $_SESSION['online'] = uniqid();
-                $ip = $_SERVER['REMOTE_ADDR'];
+                if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+                    $ip = $_SERVER['HTTP_CLIENT_IP'];
+                } else if(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+                    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+                } else{
+                    $ip = $_SERVER['REMOTE_ADDR'];
+                }
                 $token = $_SESSION['online'];
                 $horarioAtual = date('Y-m-d H:i:s');
                 $sql = MySql::conectar()->prepare("INSERT INTO `tb_admin.online` VALUES (null,?,?,?)");
